17ab903064232a36be2278295cb041d2b4a45381
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-10-21 19:22:55 +0300

    sending 200 http response

--- app/main.ts
+++ app/main.ts
@@ -4,12 +4,25 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
  //Uncomment this to pass the first stage
- const server = net.createServer((socket:any) => {
+ const server = net.createServer((socket:net.Socket) => {
    socket.on("close", () => {
      socket.end();
    });
+   socket.write(Buffer.from(`HTTP/1.1 200 OK \r\n\r\n`));
+   /**
+    * socket.write() : send data through socket connection
+    * 
+    * buffer.from() : convert string to buffer for efficient transmission
+    * 
+    * A Buffer is a raw binary data storage, which is useful for sending and receiving data over network protocols like TCP.
+    * 
+    *   
+    */
+
  });
 
  server.listen(4221, "localhost");
+
+
 
  
